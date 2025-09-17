7c9f5e487fe5122c331b629f31f525fdd6d4b12b
Kim Jiah <2024184004@tukorea.ac.kr>
2025-09-17 15:33:12 +0900

    오른쪽 벽을 따라 이동

--- character_moves.py
+++ character_moves.py
@@ -11,6 +11,9 @@ while(True):
     clear_canvas_now()
     grass.draw_now(400, 30)
     character.draw_now(x, y)
-    if(x < 780):
+    if(x < 780 and y == 90):
         x += 2
+        delay(0.01)
+    elif(y < 560 and x == 780):
+        y += 2
         delay(0.01)
