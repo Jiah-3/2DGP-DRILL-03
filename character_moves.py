913880989868e23d3f6dcf5b6a5c2f07a9ed002e
Kim Jiah <2024184004@tukorea.ac.kr>
2025-09-17 15:27:53 +0900

    캐릭터가 앞으로 이동

--- character_moves.py
+++ character_moves.py
@@ -5,8 +5,11 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-clear_canvas_now()
-grass.draw_now(400, 30)
-character.draw_now(20, 90)
-
-delay(1)
+x = 20
+y = 90
+while(True):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, y)
+    x += 2
+    delay(0.01)
