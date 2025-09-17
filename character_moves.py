5d61f3ef8b1ed5a627868d133c3048e060f52a72
Kim Jiah <2024184004@tukorea.ac.kr>
2025-09-17 16:05:42 +0900

    원 그리고 다시 사각형 그리기로 변경

--- character_moves.py
+++ character_moves.py
@@ -7,6 +7,8 @@ character = load_image('character.png')
 
 x = 20
 y = 90
+X = 400
+Y = 90
 shape = 0
 while(True):
     clear_canvas_now()
@@ -35,9 +37,20 @@ while(True):
             x -= 5
             y += 470 / 76.0
             delay(0.01)
-        elif(x <= 400 and y > 90):
+        elif(x > 20 and y > 90):
             x -= 5
             y -= 470 / 76.0
             delay(0.01)
-        if(x == 20 and y == 90):
+        if(x == 20 and y > 89 or x == 20 and y < 91):
             shape = 2
+    elif(shape == 2):
+        for degree in range(-90, 270, 5):
+            x = X + 250 * math.cos(math.radians(degree))
+            y = Y + 250 * math.sin(math.radians(degree))+250
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character.draw_now(x, y)
+            delay(0.01)
+        shape = 0
+        x = 20
+        y = 90
